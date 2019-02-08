1f0c8594555139639a4dba58230bfff344d9df92
Arghadip Chakraborty <chakrabortyarghadip5@gmail.com>
2019-02-09 01:12:35 +0530

    v 1.1

--- app.js
+++ app.js
@@ -15,22 +15,75 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-var db=mysql.createConnection ({
+// var db=mysql.createConnection ({
+//     host: 'localhost',
+//     user:'root',
+//     database: 'aarg',
+
+// })
+// db.connect(function(err){
+//     if(err){
+//         throw err;
+//     }
+//     console.log('Connected to Database!');
+// });
+// global.db=db;
+
+// let query="CREATE DATABASE arg;
+// CREATE TABLE IF NOT EXISTS `users` (
+//   `id` int(5) NOT NULL AUTO_INCREMENT,
+//   `name` varchar(255) NOT NULL,
+//   `stid` varchar(255) NOT NULL,
+//   `boos` varchar(255) NOT NULL,
+//   PRIMARY KEY (`id`)
+// ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1";
+
+// var db=mysql.createConnection ({
+//     host: 'localhost',
+//     user:'root',
+//     database: 'aarg',
+
+// })
+// db.connect(function(err){
+//     if(err){
+//         throw err;
+//     }
+//     console.log('Connected to Database!');
+// });
+// global.db=db;
+
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    
+  });
+  
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    db.query("CREATE DATABASE IF NOT EXISTS lib", function (err, result) {
+      if (err) throw err;
+      console.log("Database created");
+    });
+  });
+  global.db=db;
+  var db = mysql.createConnection({
     host: 'localhost',
-    user:'root',
-    database: 'aarg',
-
-})
-db.connect(function(err){
-    if(err){
-        throw err;
-    }
-    console.log('Connected to Database!');
-});
-global.db=db;
-
-
-
+    user: 'root',
+    database: 'lib'
+  });
+   
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE IF NOT EXISTS users (id int(5) NOT NULL AUTO_INCREMENT,name varchar(255) NOT NULL,stid varchar(255) NOT NULL,boos varchar(255) NOT NULL,PRIMARY KEY (id)) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1";
+    db.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+  });
+  global.db=db;
+ 
 app.get('/', showUser);
 app.get('/add',addUserPage);
 app.post('/add',addUser);
@@ -39,4 +92,4 @@ app.post('/edit:id',editUser);
 app.get('/delete:id',deleteUser);
 
 
-app.listen(3000);
+app.listen(8080);
